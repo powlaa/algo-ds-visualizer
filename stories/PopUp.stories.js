c57ad93a42653a8b060697d5f9b1fbd5b9617fcd
powlaa <s0558576@htw-berlin.de>
2023-03-19 20:05:20 +0100

    Add popup stories

---
+++ stories/PopUp.stories.js
@@ -0,0 +1,35 @@
+import "../src/static/css/index.css";
+import { PopUp } from "../src/static/js/components/PopUp";
+import { action } from "@storybook/addon-actions";
+
+export default {
+    title: "PopUp",
+};
+
+const Template = ({ popupWidth, popupMinWidth, popupHeight, overlay }) =>
+    `<pop-up style="--popup-width: ${popupWidth}; --popup-min-width: ${popupMinWidth}; --popup-height: ${popupHeight};" ${
+        overlay ? "overlay" : ""
+    }></pop-up>`;
+
+export const PopUpNoOverlay = Template.bind({});
+PopUpNoOverlay.args = {
+    overlay: false,
+    popupMinWidth: "20%",
+    popupWidth: "50%",
+    popupHeight: "50%",
+};
+
+export const PopUpOverlay = Template.bind({});
+PopUpOverlay.args = {
+    overlay: true,
+    popupMinWidth: "30%",
+    popupWidth: "60%",
+    popupHeight: "60%",
+};
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    const popup = document.querySelector("pop-up");
+    if (!e.isTrusted && popup) {
+        popup.show();
+    }
+});
