4c239e28f4d837b66fd6af16e0a17300038ba4fd
powlaa <s0558576@htw-berlin.de>
2023-03-19 21:00:01 +0100

    Add control panel story

---
+++ stories/ControlPanel.stories.js
@@ -0,0 +1,43 @@
+import "../src/static/css/index.css";
+import { ControlPanel } from "../src/static/js/components/ControlPanel";
+import { action } from "@storybook/addon-actions";
+
+export default {
+    title: "ControlPanel",
+};
+
+const Template = ({}) => `<control-panel></control-panel>`;
+
+export const RegularControlPanel = Template.bind({});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    const controlPanel = document.querySelector("control-panel");
+    document.querySelector("#root").style.height = "100%";
+    if (!e.isTrusted && controlPanel) {
+        controlPanel.data = [
+            {
+                name: "add",
+                parameters: [
+                    { name: "index", type: "number", min: 0 },
+                    { name: "data", type: "text" },
+                ],
+            },
+            {
+                name: "delete",
+                parameters: [{ name: "data", type: "text" }],
+            },
+            {
+                name: "contains",
+                parameters: [{ name: "data", type: "text" }],
+            },
+            {
+                name: "get",
+                parameters: [{ name: "index", type: "number", min: 0 }],
+            },
+        ];
+        controlPanel.addEventListener("add", (e) => action("add")(e.detail));
+        controlPanel.addEventListener("delete", (e) => action("delete")(e.detail));
+        controlPanel.addEventListener("contains", (e) => action("contains")(e.detail));
+        controlPanel.addEventListener("get", (e) => action("get")(e.detail));
+    }
+});
