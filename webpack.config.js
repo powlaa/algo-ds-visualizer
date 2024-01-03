ce8973194b44786be0f133e5177e8965929cff2e
FlyingBaum <kristgsus@gmail.com>
2024-01-03 22:59:34 +0100

    feat: simple ts webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "ts-loader"
+            },
+        }]
+    },
+    devtool: false,
+    resolve: {
+        extensions: [".js", ".ts", ]
+    }
+}
