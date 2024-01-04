f9288d1bf1dc7e3e6daa397285786c73594665a1
FlyingBaum <kristgsus@gmail.com>
2024-01-04 01:06:38 +0100

    feat: add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+    entry: "./src/app.ts",
     mode: "development",
     module: {
         rules: [{
@@ -9,8 +13,20 @@ module.exports = {
             },
         }]
     },
+    // Set devtool to false when in production. It is usually helpful for debugging in dev mode.
     devtool: false,
     resolve: {
         extensions: [".js", ".ts", ]
-    }
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Algorithm Visualizer",
+            filename: "index.html",
+            template: "./src/template.html",
+        }),
+    ],
 }
