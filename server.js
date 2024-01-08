6bc5cb235a6853dadcde55b88a04fd242c2d9e16
FlyingBaum <kristgsus@gmail.com>
2024-01-08 02:23:29 +0100

    refactor: serve from dist now

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "src", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("server started"));
