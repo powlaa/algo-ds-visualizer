130bef189eb904feb4146b92789a2839351883c3
FlyingBaum <kristgsus@gmail.com>
2024-01-09 17:44:45 +0100

    refactor: obsolete import of mixin

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 const importAll = (context: __WebpackModuleApi.RequireContext) =>
   context.keys().forEach(context);
 
-// Import your components, mixins, and views
-importAll(require.context("./static/code/mixins/", true, /\.(js|ts)$/));
+// Import your components, mixins, and views.
 importAll(require.context("./static/code/components/", true, /\.(js|ts)$/));
 importAll(require.context("./static/code/views/", true, /\.(js|ts)$/));
 
