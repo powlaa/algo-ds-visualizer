f00166d313ebb167a433f5c991d2b6f5d2ffe9c5
FlyingBaum <kristgsus@gmail.com>
2024-01-23 22:44:46 +0100

    feat: add pseudocode type

---
+++ src/static/code/views/types.ts
@@ -0,0 +1,17 @@
+/**
+ * Includes everything related to the code view.
+ */
+export interface Pseudocode {
+  /**
+   * The line of code.
+   */
+  code: string;
+  /**
+   * The indentation level of the code.
+   */
+  indent: number;
+  /**
+   * The label for the line of code.
+   */
+  label: string;
+}
